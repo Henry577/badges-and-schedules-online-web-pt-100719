5580d2711f2f2d91959c27c6882a88792cb84934
Henry Allan <henryallan50@gmail.com>
2019-10-23 18:04:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,69 @@
-# Write your code here.
+def badge_maker(name)
+  
+  p "Hello, my name is #{name}."
+  
+end
+
+
+
+def batch_badge_creator(names)
+  
+  count = 0
+  
+  names.each do |name|
+    
+    names[count] = badge_maker(name)
+    count+= 1
+    
+  end
+  
+end
+
+
+
+def assign_rooms(people)
+  
+  room_assignments = []
+  count = 0
+  
+  people.each do |person|
+    
+    room_assignments[count] = "Hello, #{person}! You'll be assigned to room #{count + 1}!"
+    
+    count += 1
+    
+  end
+  
+  return room_assignments
+  
+end
+
+
+
+def printer(stuff)
+  
+  default = stuff
+  
+  p default
+  
+  batch_badge_creator(stuff).each do |badge|
+    
+    puts badge
+    
+  end
+  
+  p default
+  
+  assign_rooms(default).each do |assignment|
+    
+    puts assignment
+    
+  end
+  
+end
+
+
+  
+
+
+
